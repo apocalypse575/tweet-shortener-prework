b6a7205a0dad9a023166ba0a325aa1c07721c47f
apocalypse575 <apocalypse575@gmail.com>
2018-01-08 21:55:53 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -20,7 +20,7 @@ def word_substituter(tweet)
   # replaces long words with their expected short form
 
     tweet.split(" ").map do |word|
-      if dictionary.keys.include?(word)
+      if dictionary.keys.include?(word.downcase)
         word = dictionary[word]
       else
         word
