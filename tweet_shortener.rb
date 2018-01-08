7d7bfdc6eba9efb623afc23db938a533bf28e90f
apocalypse575 <apocalypse575@gmail.com>
2018-01-08 21:08:34 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+require "pry"
+
+def dictionary
+  convert = {
+   "hello" => "hi",
+   "to" => "2",
+   "two" => "2",
+   "too" => "2",
+   "for" => "4",
+   "four" => "4",
+   "be" => "b",
+   "you" => "u",
+   "at" => "@",
+   "and" => "&"
+}
+end
+
+
+
+
+def word_substituter(tweet)
+  # replaces long words with their expected short form
+
+  dictionary.map do |keys, values|
+    tweet.split(" ").map do |words|
+    words if tweet.split(" ").include?(keys)
+  end.compact
+end
+end
