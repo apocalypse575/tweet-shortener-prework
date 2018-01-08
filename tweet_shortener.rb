fdc0652265217e14db644b89049474be2583696a
apocalypse575 <apocalypse575@gmail.com>
2018-01-08 21:11:46 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -24,7 +24,7 @@ def word_substituter(tweet)
 
   dictionary.map do |keys, values|
     tweet.split(" ").map do |words|
-    words if tweet.split(" ").include?(keys)
+    words = values if tweet.split(" ").include?(keys)
   end.compact
 end
 end
